879ca5cf6520b87ebd8d7281f995a1469210854a
NEUKhoaLe <khoadangleneu24@gmail.com>
2022-01-07 04:33:27 -0500

    Updated the code

--- Cards/PlayerDeck.py
+++ Cards/PlayerDeck.py
@@ -1,3 +1,5 @@
+from math import ceil
+
 import pygame
 
 from Cards.AbstractDeck import AbstractDeck
@@ -18,7 +20,10 @@ class PlayerDeck(AbstractDeck):
         self.collide_point = collide_point
 
         self.chosen_deck = []
+
         self.was_chosen_deck = []
+        self.to_be_chosen_cards = []
+        self.card_mid_point_y = self.y - self.card_height/2
 
         self.chosen_y = chosen_y
         self.settings = Settings()
@@ -49,17 +54,22 @@ class PlayerDeck(AbstractDeck):
                     min((num_cards * self.card_width), self.width), self.card_height)
 
         for i in range(len(self.deck)):
-            if self.was_chosen_deck.__contains__(self.deck[i]):
-                self.surface.blit(self.half_card, (starting, self.chosen_y))
-                self.was_chosen_deck.remove(self.deck[i])
-
             self.surface.blit(self.background, (starting + self.card_width, self.y))
 
             x = self.deck[i]
 
-            self.draw_rest_deck(x)
-
             if not x.get_chosen():
+                # We draw the background, then draw the previous cards, then proceed.
+                if self.was_chosen_deck.__contains__(x):
+                    width = self.get_cover_width(i, card_pos, for_chosen=False)
+                    self.half_card = pygame.Surface((width, self.card_height/2))
+                    self.half_card.fill(self.settings.bg_color)
+                    self.surface.blit(self.half_card, (starting + self.card_width - width, self.chosen_y))
+                    self.was_chosen_deck.remove(x)
+
+                self.draw_previous(i, card_pos)
+
+                self.draw_rest_deck(x)
                 x.update_vis(True)
                 if x.cur_pos()[1] == self.y or x.cur_pos()[1] == self.chosen_y:
                     x.move(starting, self.y, True)
@@ -78,6 +88,18 @@ class PlayerDeck(AbstractDeck):
 
             # Do this portion
             else:
+                if self.to_be_chosen_cards.__contains__(x):
+                    width = self.get_cover_width(i, card_pos, for_chosen=True)
+                    self.half_card = pygame.Surface((width, self.card_height/2))
+                    self.half_card.fill(self.settings.bg_color)
+                    self.surface.blit(self.half_card,
+                                      (starting + self.card_width - width, self.y + self.card_height/2))
+                    self.to_be_chosen_cards.remove(x)
+
+                self.draw_previous(i, card_pos)
+
+                self.draw_rest_deck(x)
+
                 x.move(starting, self.chosen_y, True)
                 original_x, original_y = x.cur_pos()
 
@@ -102,7 +124,8 @@ class PlayerDeck(AbstractDeck):
     def draw_rest_deck(self, card):
         index = self.deck.index(card)
         for c in range(index + 1, len(self.deck)):
-            if not (self.deck[c].get_chosen() and c == index + 1):
+            if (not (self.deck[c].get_chosen() and c == index + 1)) \
+                    or self.to_be_chosen_cards.__contains__(self.deck[c]):
                 self.deck[c].draw(still_drawing=False)
 
     def update(self, game_update):
@@ -129,7 +152,9 @@ class PlayerDeck(AbstractDeck):
         return self.x, self.y
 
     def handle_selected(self, mouse_x, mouse_y):
-        for card in self.deck:
+        i = len(self.deck) - 1
+        while i >= 0:
+            card = self.deck[i]
             if card.handle_selected(mouse_x, mouse_y):
                 if card.get_chosen():
                     self.move_to_deck(card)
@@ -139,10 +164,73 @@ class PlayerDeck(AbstractDeck):
                 else:
                     self.move_to_chosen(card)
                     card.change_chosen(True)
+                    self.to_be_chosen_cards.append(card)
                     return
+            i -= 1
 
     def move_to_deck(self, card):
         self.chosen_deck.remove(card)
 
     def move_to_chosen(self, card):
         self.chosen_deck.append(card)
+
+    def get_cover_width(self, index, card_pos, for_chosen):
+        remaining_width = self.card_width - card_pos
+        num_cards = min(index, ceil(remaining_width/card_pos))
+        min_width = card_pos
+
+        if num_cards == index:
+            i = index - 1
+            manual_break = False
+            while i >= 0:
+                card = self.deck[i]
+                if for_chosen:
+                    if card.get_chosen():
+                        min_width = min(self.card_width, min_width + card_pos)
+                    else:
+                        manual_break = True
+                        break
+                else:
+                    if not card.get_chosen():
+                        min_width = min(self.card_width, min_width + card_pos)
+                    else:
+                        manual_break = True
+                        break
+                i -= 1
+            if not manual_break:
+                if not min_width == self.card_width:
+                    min_width = self.card_width
+        else:
+            i = index - 1
+            while i >= index - num_cards:
+                card = self.deck[i]
+                if for_chosen:
+                    if card.get_chosen():
+                        min_width = min(self.card_width, min_width + card_pos)
+                    else:
+                        break
+                else:
+                    if not card.get_chosen():
+                        min_width = min(self.card_width, min_width + card_pos)
+                    else:
+                        break
+                i -= 1
+
+        return min_width
+
+    def draw_previous(self, index, card_pos):
+        remaining_width = self.card_width - card_pos
+        num_cards = min(index, ceil(remaining_width/card_pos))
+
+        if num_cards == index:
+            i = 0
+            while i < index:
+                card = self.deck[i]
+                card.draw(still_drawing=False)
+                i += 1
+        else:
+            i = index - num_cards
+            while i < index:
+                card = self.deck[i]
+                card.draw(still_drawing=False)
+                i += 1
