a6517c9b127e92c59b05cd3ab17cd0b79d3a7956
NEUKhoaLe <khoadangleneu24@gmail.com>
2022-02-04 21:13:08 -0500

    Massive Work done to progress the Online portion of the game.

--- Server2.py
+++ Server2.py
@@ -4,69 +4,88 @@ from _thread import *
 
 from Game.Player2.ServerGame2 import ServerGame2
 
-server = "192.168.1.241"
-port = 5555
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-try:
-    s.bind((server, port))
-except socket.error as e:
-    str(e)
-
-s.listen(2)
-print("Waiting for a connection, Server Started.")
-
-idCount = 0
-games = {}
+"""
+The server object. It will have methods to setup the server, start the server, and a method
+to perform threaded actions on each connections. They will also try to broadcast 
+"""
 
 
-def threaded_client(conn, player, game_id):
-    global idCount
-    conn.send(str.encode(str(player)))
+class Server2:
+    def __init__(self):
+        self.server = "192.168.1.241"
+        self.port = 5555
 
-    while True:
+        self.idCount = 0
+        self.games = {}
+        self.game_instructions = {}
+        self.connections = []
+
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        self.setup()
+        self.startup()
+
+    def setup(self):
         try:
-            data = conn.recv(2048 * 4).decode()
+            self.server_socket.bind((self.server, self.port))
+        except socket.error as e:
+            str(e)
 
-            if game_id in games:
-                game = games[game_id]
+    def startup(self):
+        self.server_socket.listen(2)
+        print("Waiting for a connection, Server Started.")
 
-                if not data:
-                    print("Disconnected.")
-                    break
-                elif data == "get":
-                    conn.send(pickle.dumps([game, player]))
-                else:
-                    pass
+        while True:
+            conn, addr = self.server_socket.accept()
+            print("Connected to: ", addr)
 
-                conn.sendall(pickle.dumps([game, player]))
+            self.connections.append(conn)
+
+            self.idCount += 1
+            player = 1
+            game_id = (self.idCount - 1) // 2
+
+            if self.idCount % 2 == 1:
+                self.games[game_id] = ServerGame2(game_id)
+                self.game_instructions[game_id] = []
+                print("Creating a new game...")
+            else:
+                self.games[game_id].set_ready(True)
+                player = 2
+
+            start_new_thread(self.threaded_client, (conn, player, game_id))
+
+    def threaded_client(self, conn, player, game_id):
+        conn.send(str.encode(str(player)))
+
+        while True:
+            try:
+                data = conn.recv(2048 * 4).decode()
+
+                if game_id in self.games:
+                    game = self.games[game_id]
+
+                    if not data:
+                        print("Disconnected.")
+                        break
+                    elif data == "get":
+                        conn.send(pickle.dumps([game, player]))
+                    else:
+                        pass
+
+                    conn.sendall(pickle.dumps([game, player]))
+            except:
+                break
+
+        print("Lost connection.")
+        try:
+            del self.games[game_id]
+            print("Closing Game", game_id)
         except:
-            break
+            pass
 
-    print("Lost connection.")
-    try:
-        del games[game_id]
-        print("Closing Game", game_id)
-    except:
-        pass
-    idCount -= 1
-    conn.close()
+        self.idCount -= 1
+        conn.close()
 
 
-while True:
-    conn, addr = s.accept()
-    print("Connected to: ", addr)
-
-    idCount += 1
-    player = 1
-    game_id = (idCount - 1) // 2
-
-    if idCount % 2 == 1:
-        games[game_id] = ServerGame2(game_id)
-        print("Creating a new game...")
-    else:
-        games[game_id].set_ready(True)
-        player = 2
-
-    start_new_thread(threaded_client, (conn, player, game_id))
+server = Server2()
